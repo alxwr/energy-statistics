48e6284d1a0141aa453aaf50f05ed6cc8e4b93cd
awildpatchappears <github@sz9i.net>
2015-08-10 14:27:10 +0200

    Added license

--- poller.py
+++ poller.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#    energy-statistics
+#    Copyright (C) 2015  Alexander Weidinger
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Contact information: Alexander Weidinger <aw@sz9i.net>
+
 import urllib2, urllib, re, argparse, sys, platform, os
 from datetime import datetime
 import pytz
